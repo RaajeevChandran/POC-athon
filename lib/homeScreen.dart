45022b26cec81a6e969c024cdbc6831fab16d083
Raajeev Chandran <raajeevchandran787@gmail.com>
2021-05-06 17:23:33 +0530

    firebase

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:votefromhome/providers/userProvider.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,8 +11,32 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    UserProvider userProvider =
+        Provider.of<UserProvider>(context, listen: true);
+    // print(userProvider.currentUser.username);
+    CollectionReference users =
+        FirebaseFirestore.instance.collection('unsignedVC');
+
     return Scaffold(
-      body:Center(child: Text("Home Screen"),)
+      body: FutureBuilder<QuerySnapshot>(
+        future: users.get(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Something went wrong");
+          }
+
+          if (snapshot.hasData && !snapshot.hasData) {
+            return Text("Document does not exist");
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            // Map<String, dynamic> data = snapshot.data.data();
+            return Text(snapshot.data.docs[0].data()["username"]);
+          }
+
+          return Text("loading");
+        },
+      ),
     );
   }
 }
